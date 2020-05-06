715b21ffa8bfcba5928b453d8570c895a67b049e
sankaranam <karanam@iiasa.ac.at>
2020-05-06 11:48:05 +0200

    updated path

--- Nunit.Demo/ExecuteTest.ps1
+++ Nunit.Demo/ExecuteTest.ps1
@@ -1,6 +1,8 @@
 
 # before running this file make sure all the nuggets in solution are resported and packages folder path is updated.
-$rawCoverageFolder = "C:\code\nunit-demo\Nunit.Demo\Nunit.Demo.Test\TestResults";
+$rawCoverageFolder = "$PSScriptRoot\Nunit.Demo.Test\TestResults";
+
+dotnet build Nunit.Demo.sln
 
 # delete old results
 Remove-Item -Path C:\code\nunit-demo\Nunit.Demo\Reports -Recurse -Force
@@ -19,10 +21,10 @@ $recentCoverageFile = Get-ChildItem -File -Filter *.coverage -Path $rawCoverageF
 C:\Users\hrida\.nuget\packages\microsoft.codecoverage\16.5.0\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:C:\code\nunit-demo\Nunit.Demo\Reports\coverage\coverage.xml $recentCoverageFile.FullName
 
 #copy xml TestResults.xml report to results folder
-Copy-Item -Path C:\code\nunit-demo\Nunit.Demo\Nunit.Demo.Test\TestResults\TestResults.xml -Destination C:\code\nunit-demo\Nunit.Demo\Reports
+Copy-Item -Path "$PSScriptRoot\Nunit.Demo.Test\TestResults\TestResults.xml" -Destination "$PSScriptRoot\Reports"
 
 #Generate html coverage report
-C:\Users\hrida\.nuget\packages\reportgenerator\4.5.6\tools\netcoreapp3.0\ReportGenerator.exe -reports:C:\code\nunit-demo\Nunit.Demo\Reports\coverage\coverage.xml -vernosity:Info -targetdir:C:\code\nunit-demo\Nunit.Demo\Reports\coverage
+C:\Users\hrida\.nuget\packages\reportgenerator\4.5.6\tools\netcoreapp3.0\ReportGenerator.exe -reports:"$PSScriptRoot\Reports\coverage\coverage.xml" -vernosity:Info -targetdir:"$PSScriptRoot\Reports\coverage"
 
 #Generate html test cases report
 .\extent.exe -i .\Reports\TestResults.xml -o .\Reports
